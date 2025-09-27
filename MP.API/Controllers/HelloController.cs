5421f984f0df86a06ec086b36b2e4938a5b87a28
MrMaheshPawar-github <maheshpawar997069@gmail.com>
2025-09-27 15:09:17 +0530

    #Added Controller for Test API

---
+++ MP.API/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MP.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string get()
+        {
+            return "Hello MP , Your Web Api Is Working well!!";
+        }
+    }
+}
---
+++ MP.API/README.md
@@ -0,0 +1,7 @@
+# MP.com Project
+This is my learning journey project using .NET Full Stack.
+
+## Tech Stack
+- Backend: ASP.NET Core Web API
+- Frontend: React 
+- Database: SQL Server
